934595c773e09c1a101befd729a5c7ab2c89390d
= <=>
2020-11-05 11:36:08 +0100

    Initial commit

---
+++ test.py
@@ -0,0 +1,7 @@
+print("Hello World!") # utilisation litéral
+
+message = "Hello World!" # déclaration de la variable message en utilisant "="
+
+print(message) # utilisation de la variable comme argument à la fonction "print"
+
+# print(mesage)
